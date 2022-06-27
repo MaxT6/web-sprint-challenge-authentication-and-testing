09ba23961a3a15669da741a99f5b8002d78055f0
MaxT6 <maximiliant6@gmail.com>
2022-06-27 17:03:46 -0400

    debugged jokes endpoint

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,16 +1,20 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../secrets/index.js');
+
 const restrict = (req, res, next) => {
+
   const token = req.headers.authorization
-  if (!token) {
-    return next({ status: 401, message: "token required"})
-  } 
-   jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
-        if(err) {
-            next({ status: 401, message: "token invalid" })
-        } else {
-            req.decodedToken = decodedToken
-            next()
-        }
-    })
+      if (!token) {
+        return next({ status: 401, message: "token required"})
+      } 
+       jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+            if(err) {
+                next({ status: 401, message: "token invalid" })
+            } else {
+                req.decodedToken = decodedToken
+                next()
+            }
+        })
   /*
     IMPLEMENT
 
